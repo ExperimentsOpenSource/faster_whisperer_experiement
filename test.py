50afd07fce977c88c659d621103056974bd2f23b
ThammanaSrinivas <sreenivast84@gmail.com>
2025-08-20 07:18:31 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from faster_whisper import WhisperModel
+
+model_size = "medium.en"
+
+# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+
+segment, _ = model.transcribe("test.mp3", language="en", beam_size=5)
+
+for segment in segment:
+    print(segment.text)
